df0a125a2512b37323e24ce1fd049edd273f7ccb
ktmihs <yellowgreen423@gmail.com>
2021-11-18 14:04:14 +0900

    ✨ feat: create overall layout and style

--- src/js/banner.js
+++ src/js/banner.js
@@ -1,62 +1,62 @@
-const prevBtn = document.querySelector('.button-prev');
-const nextBtn = document.querySelector('.button-next');
-const pauseBtn = document.querySelector('.button-pause');
-let state = document.querySelector('.current-page');
-let slides = document.querySelectorAll('.banner-slide');
+const prevBtn = document.querySelector(".button-prev");
+const nextBtn = document.querySelector(".button-next");
+const pauseBtn = document.querySelector(".button-pause");
+let state = document.querySelector(".current-page");
+let slides = document.querySelectorAll(".banner-slide");
 let currentSlide = 0;
-let slideInterval = setInterval(showSlides, 1000);
+let slideInterval = setInterval(showSlides, 5000);
 let playing = true;
 
 function showSlides() {
-  slides[currentSlide].classList.remove('is-active');
+  slides[currentSlide].classList.remove("is-active");
   currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].classList.add('is-active');
+  slides[currentSlide].classList.add("is-active");
   state.innerHTML = currentSlide + 1;
 }
 
 function pauseSlide() {
-  pauseBtn.innerHTML = '>';
+  pauseBtn.innerHTML = ">";
   playing = false;
   clearInterval(slideInterval);
 }
 
 function playSlide() {
-  pauseBtn.innerHTML = '||';
+  pauseBtn.innerHTML = "||";
   playing = true;
-  slideInterval = setInterval(showSlides, 1000);
+  slideInterval = setInterval(showSlides, 5000);
 }
 
-pauseBtn.addEventListener('click', function () {
+pauseBtn.addEventListener("click", function () {
   if (playing) pauseSlide();
   else playSlide();
 });
 
-nextBtn.addEventListener('click', function () {
+nextBtn.addEventListener("click", function () {
   if (playing) {
     clearInterval(slideInterval);
-    slides[currentSlide].classList.remove('is-active');
+    slides[currentSlide].classList.remove("is-active");
     currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add('is-active');
-    slideInterval = setInterval(showSlides, 1000);
+    slides[currentSlide].classList.add("is-active");
+    slideInterval = setInterval(showSlides, 5000);
   } else {
-    slides[currentSlide].classList.remove('is-active');
+    slides[currentSlide].classList.remove("is-active");
     currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add('is-active');
+    slides[currentSlide].classList.add("is-active");
   }
   state.innerHTML = currentSlide + 1;
 });
 
-prevBtn.addEventListener('click', function () {
+prevBtn.addEventListener("click", function () {
   if (playing) {
     clearInterval(slideInterval);
-    slides[currentSlide].classList.remove('is-active');
+    slides[currentSlide].classList.remove("is-active");
     currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-    slides[currentSlide].classList.add('is-active');
-    slideInterval = setInterval(showSlides, 1000);
+    slides[currentSlide].classList.add("is-active");
+    slideInterval = setInterval(showSlides, 5000);
   } else {
-    slides[currentSlide].classList.remove('is-active');
+    slides[currentSlide].classList.remove("is-active");
     currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-    slides[currentSlide].classList.add('is-active');
+    slides[currentSlide].classList.add("is-active");
   }
   state.innerHTML = currentSlide + 1;
 });
